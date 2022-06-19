3d038e5863c4197566776666ae83c4b2fd86eedd
zcy <25912591@qq.com>
2022-06-19 18:14:48 +0800

    First Idea Test

--- src/git/Test/com/GitTest.java
+++ src/git/Test/com/GitTest.java
@@ -3,6 +3,8 @@ package git.Test.com;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hot-fix");
+        System.out.println("merge branch");
     }
 
 }
