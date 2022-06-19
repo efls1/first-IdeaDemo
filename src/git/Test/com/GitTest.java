b4aa260817a59b0a00458fd9d39c593fbb740280
zcy <25912591@qq.com>
2022-06-19 18:35:00 +0800

    Master  Second Idea Test

--- src/git/Test/com/GitTest.java
+++ src/git/Test/com/GitTest.java
@@ -2,7 +2,12 @@ package git.Test.com;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("merge branch");
+        System.out.println("merge branch");
+        System.out.println("merge branch");
+        System.out.println("merge branch");
+        System.out.println("merge branch");
+        System.out.println("merge branch");
     }
 
 }
